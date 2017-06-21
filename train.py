fc82f5ce65d19665a7a7f5f2ff1cbc79490a186e
leVirve <gae.m.project@gmail.com>
2017-06-22 02:12:58 +0800

    update training fit_gen parameters

--- train.py
+++ train.py
@@ -13,6 +13,10 @@ from fcn_models.fcn_score import (
 def main():
 
     size = 512
+    workers = 32
+    epochs = 2
+    batch_size = 16
+
     data_gen = DataGenerator()
     train_generator = data_gen.flow_from_directory(
         directory='../data', phase=Phase.TRAIN,
@@ -23,15 +27,22 @@ def main():
         target_size=(size, size),
         batch_size=4)
 
-    model = fcn_vggbase(pretrained_weights='../model_June13_sgd_60kitr.h5')
-
+    model = fcn_vggbase(
+        input_shape=(size, size, 3),
+        pretrained_weights='../model_June13_sgd_60kitr.h5')
+    model.summary()
     model.compile(
         optimizer=RMSprop(lr=1e-4),
         loss=[softmax_sparse_crossentropy_ignoring_last_label],
         metrics=[sparse_accuracy_ignoring_last_label])
     model.fit_generator(
-        generator=train_generator, steps_per_epoch=20, epochs=2,
-        validation_data=validation_generator, validation_steps=10)
+        generator=train_generator,
+        steps_per_epoch=(train_generator.samples + 1) // batch_size,
+        validation_data=validation_generator,
+        validation_steps=(validation_generator.samples + 1) // batch_size,
+        epochs=epochs,
+        workers=workers
+    )
 
 
 if __name__ == '__main__':
