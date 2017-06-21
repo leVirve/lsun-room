c9ba2905f3b55952fb6290d71216973c5b1a2832
leVirve <gae.m.project@gmail.com>
2017-06-22 03:20:48 +0800

    add callbacks for training

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
-from keras.optimizers import RMSprop
+import os
+import keras
 
 from lsun_room import Phase
 from dataset import DataGenerator
@@ -10,29 +11,44 @@ from fcn_models.fcn_score import (
 )
 
 
+optimizer = keras.optimizers.RMSprop(lr=1e-4)
+
+callbacks = [
+    keras.callbacks.CSVLogger('output/training.log'),
+    keras.callbacks.ReduceLROnPlateau(
+        monitor='val_loss', factor=0.5, patience=3, verbose=1, min_lr=1e-8),
+    keras.callbacks.ModelCheckpoint(
+        filepath='output/weights.{epoch:02d}.hdf5',
+        verbose=1, save_best_only=True, save_weights_only=True),
+    keras.callbacks.TensorBoard(
+        log_dir='./logs', batch_size=32, write_graph=False)
+]
+
+
 def main():
 
     size = 512
-    workers = 32
-    epochs = 2
-    batch_size = 16
+    workers = 16
+    epochs = 50
+    batch_size = 8
 
     data_gen = DataGenerator()
     train_generator = data_gen.flow_from_directory(
         directory='../data', phase=Phase.TRAIN,
         target_size=(size, size),
-        batch_size=4, shuffle=True)
+        batch_size=batch_size, shuffle=True)
     validation_generator = data_gen.flow_from_directory(
         directory='../data', phase=Phase.VALIDATE,
         target_size=(size, size),
-        batch_size=4)
+        batch_size=batch_size)
+    os.makedirs('output/', exist_ok=True)
 
     model = fcn_vggbase(
         input_shape=(size, size, 3),
         pretrained_weights='../model_June13_sgd_60kitr.h5')
     model.summary()
     model.compile(
-        optimizer=RMSprop(lr=1e-4),
+        optimizer=optimizer,
         loss=[softmax_sparse_crossentropy_ignoring_last_label],
         metrics=[sparse_accuracy_ignoring_last_label])
     model.fit_generator(
@@ -41,7 +57,8 @@ def main():
         validation_data=validation_generator,
         validation_steps=(validation_generator.samples + 1) // batch_size,
         epochs=epochs,
-        workers=workers
+        workers=workers,
+        callbacks=callbacks
     )
 
 
