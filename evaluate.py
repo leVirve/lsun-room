fcd32559aa61cf9889601bba6ee4e2a569b7624a
leVirve <gae.m.project@gmail.com>
2017-06-29 00:34:00 +0800

    update evaluation script

--- evaluate.py
+++ evaluate.py
@@ -1,17 +1,19 @@
 import os
+import scipy
 import numpy as np
+import skimage.io
+import skimage.color
 
-from lsun_room import Phase
-from dataset import DataGenerator
-
+import click
+from lsun_room import Phase, Dataset
 from fcn_models.fcn32s_vgg16 import fcn_vggbase
 
 
-def bitget(byteval, idx):
-    return ((byteval & (1 << idx)) != 0)
-
-
 def labelcolormap(N=256):
+
+    def bitget(byteval, idx):
+        return ((byteval & (1 << idx)) != 0)
+
     cmap = np.zeros((N, 3))
     for i in range(0, N):
         id = i
@@ -28,40 +30,53 @@ def labelcolormap(N=256):
     return cmap
 
 
-def main():
-    experiment_name = 'vggbase_go_adam_lr1e-4'
-    weights_name = 'weights.{epoch:02d}.hdf5'.format(epoch=18)
-
+@click.command()
+@click.argument('weight_path', type=click.Path(exists=True))
+@click.option('--max_length', type=int, default=2048)
+def main(weight_path, max_length):
     dataset_root = '../data'
-    size = 512
-    workers = 16
-    batch_size = 8
 
-    data_gen = DataGenerator()
-    validation_generator = data_gen.flow_from_directory(
-        directory=dataset_root, phase=Phase.VALIDATE,
-        target_size=(size, size),
-        batch_size=batch_size)
+    experiment_name = weight_path.split('/')[-2]
+    images_folder = 'output_images/%s/' % experiment_name
+    layout_folder = 'output_layout/%s/' % experiment_name
+    os.makedirs(images_folder, exist_ok=True)
+    os.makedirs(layout_folder, exist_ok=True)
 
-    model = fcn_vggbase(
-        input_shape=(size, size, 3),
-        pretrained_weights=os.path.join(experiment_name, weights_name))
+    model = fcn_vggbase(pretrained_weights=weight_path, crop=False)
 
-    result = model.predict_generator(
-        validation_generator,
-        steps=(validation_generator.samples + 1) // batch_size,
-        workers=workers
-    )
+    cmap = labelcolormap(5)
 
-    for e in result:
-        import skimage.io
-        import skimage.color
-        cmap = labelcolormap(5)
+    dataset = Dataset(root_dir=dataset_root, phase=Phase.VALIDATE)
+    images = [e.image for e in dataset.items]
 
-        out = skimage.color.label2rgb(np.argmax(e, axis=2), colors=cmap[1:], bg_label=0)
-        skimage.io.imsave('e-out.png', np.argmax(e, axis=2))
-        skimage.io.imsave('out.png', out)
-        break
+    for img, e in zip(images, dataset.items):
+        h, w, _ = img.shape
+        if h > w and h > max_length:
+            h, w = max_length, int(w * float(max_length) / h)
+        elif w > h and w > max_length:
+            h, w = int(h * float(max_length) / w), max_length
+
+        img = scipy.misc.imresize(img, (h, w))
+
+        batched_img = np.expand_dims(img, axis=0)
+        pred = model.predict(batched_img)[0, ...]
+
+        pred_img = np.argmax(pred, axis=2)
+        print('... origin >> (%d, %d)' % (h, w))
+        print('... predict >>', pred_img.shape)
+        x = (pred_img.shape[0] - h) // 2
+        y = (pred_img.shape[1] - w) // 2
+        pred_img = pred_img[x:x + h, y:y + w]
+        print('... cropped >>', pred_img.shape)
+
+        out = skimage.color.label2rgb(pred_img, colors=cmap[1:], bg_label=0)
+
+        pred_img = scipy.misc.imresize(pred_img, (h, w))
+        out = scipy.misc.imresize(out, (h, w), interp='nearest')
+        skimage.io.imsave(images_folder + '%s.png' % e.name, out)
+        skimage.io.imsave(layout_folder + '%s.png' % e.name, pred_img)
+
+        print('--> Done', e.name)
 
 
 if __name__ == '__main__':
