3e03b020e7593a8e9b77c95dafe29f296843f472
leVirve <gae.m.project@gmail.com>
2017-07-07 17:13:20 +0800

    update main script for pytorch

--- train.py
+++ train.py
@@ -1,54 +1,93 @@
-import os
+import click
 import numpy as np
 
+import torch
+import torch.nn.functional as F
+from torch.autograd import Variable
+from tqdm import tqdm
+
 import config as cfg
+from fcn import FCN
+from dataset import ImageFolderDataset
 from lsun_room import Phase
-from dataset import DataGenerator
 
 
-from fcn import (
-    fcn32s,
-    sparse_pixelwise_accuracy,
-    corssentropy2d
-)
+def cross_entropy2d(pred, target, weight=None, size_average=True):
+    n, c, h, w = pred.size()
+
+    log_p = F.log_softmax(pred)
+    log_p = log_p.transpose(1, 2).transpose(2, 3).contiguous().view(-1, c)
+    target = target.transpose(1, 2).transpose(2, 3).contiguous().view(-1)
+
+    loss = F.nll_loss(log_p, target, weight=weight, size_average=False)
+
+    if size_average:
+        loss /= (h * w * n)
+    return loss
 
 
-def main():
-    os.makedirs('output/' + cfg.experiment_name, exist_ok=True)
-    print('==> <Experiment>:', cfg.experiment_name)
+def sparse_pixelwise_accuracy(pred, target):
+    n, num_classes, h, w = pred.size()
 
-    data_gen = DataGenerator()
-    train_generator = data_gen.flow_from_directory(
-        directory=cfg.dataset_root, phase=Phase.TRAIN,
+    _pred = pred.transpose(1, 2).transpose(2, 3).contiguous().view(-1, num_classes)
+    _target = target.view(-1)
+
+    _pred = _pred.data.max(1)[1]
+    correct = _pred.eq(_target).sum()
+
+    return correct / (h * w * n)
+
+
+@click.command()
+@click.option('--resume', type=click.Path(exists=True))
+def main(resume):
+
+    print('===> Loading dataset')
+    train_dataset = ImageFolderDataset(
+        root=cfg.dataset_root,
         target_size=(cfg.size, cfg.size),
-        batch_size=cfg.batch_size, shuffle=True)
-    validation_generator = data_gen.flow_from_directory(
-        directory=cfg.dataset_root, phase=Phase.VALIDATE,
-        target_size=(cfg.size, cfg.size),
-        batch_size=1)
+        phase=Phase.TRAIN)
 
-    validation_data = [
-        next(validation_generator)
-        for _ in range(validation_generator.samples)]
-    val_data_X = np.array([e[0][0] for e in validation_data])
-    val_data_Y = np.array([e[1][0] for e in validation_data])
+    print('===> Prepare data loader')
+    train_loader = torch.utils.data.DataLoader(
+        dataset=train_dataset,
+        batch_size=cfg.batch_size,
+        num_workers=cfg.workers,
+        pin_memory=True,
+        shuffle=True)
 
-    model = fcn32s(
-        input_shape=(cfg.size, cfg.size, 3), num_class=5,
-        weights=cfg.initial_weight_path)
-    model.summary()
-    model.compile(
-        optimizer=cfg.optimizer,
-        loss=[corssentropy2d],
-        metrics=[sparse_pixelwise_accuracy])
-    model.fit_generator(
-        generator=train_generator,
-        steps_per_epoch=(train_generator.samples + 1) // cfg.batch_size,
-        validation_data=(val_data_X, val_data_Y),
-        epochs=cfg.epochs,
-        workers=cfg.workers,
-        callbacks=cfg.callbacks
-    )
+    print('===> Prepare model')
+    model = FCN(num_classes=5)
+
+    print('===> Compile model')
+    model.cuda()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+
+    print('===> Start training')
+    losses, accuracies = np.zeros(len(train_loader)), np.zeros(len(train_loader))
+    for epoch in range(1, cfg.epochs + 1):
+        epoch_range = tqdm(train_loader)
+        for i, (img, lbl) in enumerate(epoch_range):
+            optimizer.zero_grad()
+
+            img = Variable(img).cuda()
+
+            pred = model(img)
+            loss = cross_entropy2d(pred, Variable(lbl).cuda())
+
+            loss.backward()
+            optimizer.step()
+
+            accuracy = sparse_pixelwise_accuracy(pred, lbl.cuda())
+
+            losses[i], accuracies[i] = loss.data[0], accuracy
+            avg_loss = np.mean(losses[losses > 0])
+            avg_accuracy = np.mean(accuracies[accuracies > 0])
+
+            epoch_range.set_description('Epoch# %i' % epoch)
+            epoch_range.set_postfix(
+                loss=loss.data[0], accuracy=accuracy,
+                avg_loss=avg_loss, avg_accuracy=avg_accuracy)
 
 
 if __name__ == '__main__':
