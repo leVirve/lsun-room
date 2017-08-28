524731c25fe24df20eb6eecd4b1b8a1be2703ada
leVirve <gae.m.project@gmail.com>
2017-08-28 15:30:17 +0800

    merge fcn selective base model into single module

--- models/fcn.py
+++ models/fcn.py
@@ -1,6 +1,3 @@
-import types
-
-import torchvision
 import torch.nn as nn
 import torch.nn.init as weight_init
 import torchvision.models as models
@@ -8,156 +5,36 @@ import torchvision.models as models
 from tools import timeit
 
 
-@timeit
-def build_FCN(num_classes=5, base_net='vgg16', input_size=None, pretrained=True):
-    base = getattr(models, base_net)
-    vgg = base(pretrained=pretrained)
-    classifier, upscore = score_modules(num_classes)
-
-    initialize_module(vgg, classifier)
-
-    ''' Model assmbly '''
-    vgg.output_size = input_size
-    vgg.classifier = classifier
-    vgg.upscore = upscore
-    vgg.forward = types.MethodType(gg_forward, vgg)
-
-    return vgg
-
-
-def gg_forward(self, x):
-    x = self.features(x)
-    x = self.classifier(x)
-    x = self.upscore(x)
-
-    _, _, h, w = x.size()
-    oh, ow = self.output_size
-    sh, sw = (h - oh) // 2, (w - ow) // 2
-
-    x = x[:, :, sh:-sh, sw:-sw].contiguous()
-
-    return x
-
-
-def score_modules(num_classes):
-    classifier = nn.Sequential(
-            # fc6
-            nn.Conv2d(512, 4096, 7),
-            nn.ReLU(inplace=True),
-            nn.Dropout2d(),
-
-            # fc7
-            nn.Conv2d(4096, 4096, 1),
-            nn.ReLU(inplace=True),
-            nn.Dropout2d(),
-
-            # score_fr
-            nn.Conv2d(4096, num_classes, 1),
-        )
-    upscore = nn.ConvTranspose2d(num_classes, num_classes, 64, stride=32,
-                                 bias=False)
-    return classifier, upscore
-
-
-def initialize_module(vgg, classifier):
-    vgg.features[0].padding = (100, 100)
-
-    for m in vgg.modules():
-        if isinstance(m, nn.ConvTranspose2d):
-            m.weight.data = weight_init.kaiming_normal(m.weight.data)
-        if isinstance(m, nn.MaxPool2d):
-            m.ceil_mode = True
-    for i in [0, 3]:
-        a, b = vgg.classifier[i], classifier[i]
-        b.weight.data = a.weight.data.view(b.weight.size())
-        b.bias.data = a.bias.data.view(b.bias.size())
-
-
 class FCN(nn.Module):
 
-    def __init__(self, num_classes=5, input_size=None, pretrained=True):
-        super(FCN, self).__init__()
-        self.features = nn.Sequential(
-            # conv1
-            nn.Conv2d(3, 64, 3, padding=100),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(64, 64, 3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2, stride=2, ceil_mode=True),  # 1/2
+    def __init__(self, num_classes=5, input_size=None,
+                 pretrained=True, base='vgg16_bn'):
+        super().__init__()
+        self.output_size = input_size
 
-            # conv2
-            nn.Conv2d(64, 128, 3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(128, 128, 3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2, stride=2, ceil_mode=True),  # 1/4
+        BaseNet = getattr(models, base)
+        self.vgg = BaseNet(pretrained=pretrained)
 
-            # conv3
-            nn.Conv2d(128, 256, 3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, 3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, 3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2, stride=2, ceil_mode=True),  # 1/8
-
-            # conv4
-            nn.Conv2d(256, 512, 3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, 3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, 3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2, stride=2, ceil_mode=True),  # 1/16
-
-            # conv5
-            nn.Conv2d(512, 512, 3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, 3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, 3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2, stride=2, ceil_mode=True),  # 1/32
-        )
         self.classifier = nn.Sequential(
-            # fc6
-            nn.Conv2d(512, 4096, 7),
-            nn.ReLU(inplace=True),
-            nn.Dropout2d(),
-
-            # fc7
-            nn.Conv2d(4096, 4096, 1),
-            nn.ReLU(inplace=True),
-            nn.Dropout2d(),
-
-            # score_fr
-            nn.Conv2d(4096, num_classes, 1),
-        )
+                # fc6
+                nn.Conv2d(512, 4096, 7),
+                nn.ReLU(inplace=True),
+                nn.Dropout2d(),
+                # fc7
+                nn.Conv2d(4096, 4096, 1),
+                nn.ReLU(inplace=True),
+                nn.Dropout2d(),
+                # score_fr
+                nn.Conv2d(4096, num_classes, 1),
+            )
         self.upscore = nn.ConvTranspose2d(num_classes, num_classes, 64, stride=32,
                                           bias=False)
-        self.output_size = input_size
-        if pretrained:
-            self._initialize_weights()
-
-    @timeit
-    def _initialize_weights(self):
-        vgg16 = torchvision.models.vgg16(pretrained=True)
-
-        for m in self.modules():
-            if isinstance(m, nn.ConvTranspose2d):
-                m.weight.data = weight_init.kaiming_normal(m.weight.data)
-        for a, b in zip(vgg16.features, self.features):
-            if (isinstance(a, nn.Conv2d) and isinstance(b, nn.Conv2d)):
-                b.weight.data = a.weight.data
-                b.bias.data = a.bias.data
-        for i in [0, 3]:
-            a, b = vgg16.classifier[i], self.classifier[i]
-            b.weight.data = a.weight.data.view(b.weight.size())
-            b.bias.data = a.bias.data.view(b.bias.size())
+        self._initialize_module(pretrained)
+        self._model_sugury()
 
     def forward(self, x):
-        x = self.features(x)
-        x = self.classifier(x)
+        x = self.vgg.features(x)
+        x = self.vgg.classifier(x)
         x = self.upscore(x)
 
         _, _, h, w = x.size()
@@ -165,4 +42,28 @@ class FCN(nn.Module):
         sh, sw = (h - oh) // 2, (w - ow) // 2
 
         x = x[:, :, sh:-sh, sw:-sw].contiguous()
+
         return x
+
+    @timeit
+    def _initialize_module(self, pretrained):
+        if not pretrained:
+            return
+
+        for i in [0, 3]:
+            a, b = self.vgg.classifier[i], self.classifier[i]
+            b.weight.data = a.weight.data.view(b.weight.size())
+            b.bias.data = a.bias.data.view(b.bias.size())
+
+        for m in self.modules():
+            if isinstance(m, nn.ConvTranspose2d):
+                m.weight.data = weight_init.kaiming_normal(m.weight.data)
+
+    def _model_sugury(self):
+        ''' <Black magic> Hack the netwrok structure '''
+        for m in self.vgg.modules():
+            if isinstance(m, nn.MaxPool2d):
+                m.ceil_mode = True
+
+        self.vgg.features[0].padding = (100, 100)
+        self.vgg.classifier = self.classifier
--- test/test_fcn.py
+++ test/test_fcn.py
@@ -3,26 +3,27 @@ import unittest
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
-from models.fcn import build_FCN, FCN
+from models.fcn import FCN
 
 
 class TestFCN(unittest.TestCase):
 
     image_size = (404, 404)
-
-    model = nn.DataParallel(FCN(input_size=image_size))
-    bn_model = nn.DataParallel(build_FCN(base_net='vgg16_bn', input_size=image_size))
+    fake_img = torch.randn((1, 3, *image_size)).cuda()
 
     def test_model(self):
-        fake_img = torch.randn((1, 3, 404, 404))
-        output = self.model(Variable(fake_img, requires_grad=False))
-        self.assertEqual(output.size()[1], 5)
-        self.assertEqual(output.size()[2], 404)
-        self.assertEqual(output.size()[3], 404)
+        model = nn.DataParallel(
+            FCN(input_size=self.image_size, base='vgg16')).cuda()
+        output = model(Variable(self.fake_img, requires_grad=False))
+        self._check_output_size(output)
 
     def test_bn_model(self):
-        fake_img = torch.randn((1, 3, 404, 404))
-        output = self.bn_model(Variable(fake_img, requires_grad=False))
+        model = nn.DataParallel(
+            FCN(input_size=self.image_size, base='vgg16_bn')).cuda()
+        output = model(Variable(self.fake_img, requires_grad=False))
+        self._check_output_size(output)
+
+    def _check_output_size(self, output):
         self.assertEqual(output.size()[1], 5)
         self.assertEqual(output.size()[2], 404)
         self.assertEqual(output.size()[3], 404)
