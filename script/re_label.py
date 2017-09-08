e5df0b188dbcfbbd669f19bf8f3ca632c417d448
leVirve <gae.m.project@gmail.com>
2017-09-08 15:21:18 +0800

    update script tools

--- script/re_label.py
+++ script/re_label.py
@@ -1,22 +1,25 @@
 import hack_path  # noqa
-import sys
 import time
 from multiprocessing import Pool, cpu_count
 
-from lsun_room import Phase, DataItems
+import click
+
+from datasets.lsun_room.item import DataItems
 
 
 def worker(item):
     item.remap_layout()
-    item.save_layout(visualization=True if len(sys.argv) > 1 else False)
+    item.save_layout()
 
 
-def main():
+@click.command()
+@click.option('--dataset_root', default='../data/lsun_room/')
+def main(dataset_root):
 
-    for phase in [Phase.TRAIN, Phase.VALIDATE]:
+    for phase in ['train', 'val']:
         print('==> re-label for data in %s phase' % phase)
         s = time.time()
-        dataset = DataItems(root_dir='../data', phase=phase)
+        dataset = DataItems(root_dir=dataset_root, phase=phase)
         with Pool(cpu_count()) as pool:
             pool.map(worker, dataset.items)
         print('==> Done in %.4f sec.' % (time.time() - s))
