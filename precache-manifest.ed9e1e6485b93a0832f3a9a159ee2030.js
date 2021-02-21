19ab4a610b300b1327cb3c3815db32cdd034414f
salas <gae.m.project@gmail.com>
2021-02-22 00:16:59 +0800

    Updates

---
+++ precache-manifest.ed9e1e6485b93a0832f3a9a159ee2030.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "940c3081fbe9b799121b1db7d1ee63ad",
+    "url": "/lsun-room/index.html"
+  },
+  {
+    "revision": "33a6d2bdb0a20e6ac7f1",
+    "url": "/lsun-room/static/js/2.66e49f1f.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/lsun-room/static/js/2.66e49f1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4690632a93d01798a813",
+    "url": "/lsun-room/static/js/main.6bb4aa2f.chunk.js"
+  },
+  {
+    "revision": "c9cfff49b8cf14f85d24",
+    "url": "/lsun-room/static/js/runtime-main.b3f2fd6d.js"
+  }
+]);
